79e7c0e58539d10a4925024c05ed1bf386b7e714
Hops2211 <rjohns42@hawk.iit.edu>
2017-02-08 17:19:10 -0600

    added a second toggle function to make the second form visible only when clicked

--- site.js
+++ site.js
@@ -1,10 +1,24 @@
-$.noConflict();
-  jQuery(function($)  {
-     
-     $('#information').toggle('visible');
-     
-     $('#sign-up').on('submit', function() {
-        $('#information').toggle('visible');
+ $.noConflict();
+jQuery(function($)  {
+   
+    $('#information').toggle('visible');
+    $('#information2').toggle('visible');
+      
+    $('#sign-up').on('submit', function() {
+      $('#information').toggle('visible');
+          
        
-     })
+    $('#sign-up2').on('submit', function() {
+      $('#information2').toggle('visible');
+   
+/*    $('#information').removeClass('active');
+    $('#sign-up').on('submit', function(e) {
+     $('#stuff').append(
+     'This has been clicked'
+     );
+     
+   $('#sign-up').on('click', function() {
+      $('#information').addClass('active');
+*/  })   
    })
+ })
