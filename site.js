cddecc177b06e2059b6c75cc2124d48a577baff1
Hops2211 <rjohns42@hawk.iit.edu>
2017-02-08 21:42:58 -0600

    added a function for appending the second sign up

--- site.js
+++ site.js
@@ -29,7 +29,7 @@ jQuery(function($)  {
 */  
    })
    
-   /*Take the information entered to display a message */
+   /*Takes the information entered to display a message */
    $('#information').on('submit', function(e) {
      e.preventDefault();
      var first = $('#fname').val();
@@ -38,6 +38,22 @@ jQuery(function($)  {
      $('#information').empty();
      $('#confirmation').append(
         'Thank you ' + first + ' ' + last + '.\n'
+        + 'The form was completed successfully. A confirmation email has been sent to ' +email
+      );
+     
+   })
+   
+   $('#information2').on('submit', function(e) {
+     e.preventDefault();
+     var first = $('#fname2').val();
+     var last = $('#lname2').val();
+     var cfirst = $('#cfname2').val();
+     var clast = $('#clname2').val();
+     var email = $('#email2').val();
+     $('#information2').empty();
+     $('#confirmation2').append(
+        'Thank you ' + first + ' ' + last + '.\n'
+        +'Your child, ' + cfirst + ' ' + clast + ' is now registered for class in the ' + 'room.\n' 
         + 'The form was completed successfully. A confirmation email has been sent to ' +email
       );
      
